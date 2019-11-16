5527bdf84d9eaf7a95d67c264c21d86d4ec836f3
Adam Melkus <adam@melkus.info>
2019-11-16 23:41:19 +0100

    Update nvm-lazy.plugin.zsh

--- nvm-lazy.plugin.zsh
+++ nvm-lazy.plugin.zsh
@@ -1,14 +1,18 @@
 # Lazy load nvm, with multiple entrypoints
 # Add more entrypoints by defining the ZSH_LAZY_NVM_BINARIES array
-function add_lazy() {
-    for binary in $1;
+add_lazy() {
+    function_name=$1
+    shift 1
+    rest_of_args=("$@")
+    for binary in "${rest_of_args}"
     do
+        echo "defining ${binary}"
         $binary() {
-            for FUNCTION in $1
+            for FUNCTION in $rest_of_args
             do
                 unset -f $FUNCTION
             done
-            $2()
+            $function_name()
             $binary $@
         }
     done
